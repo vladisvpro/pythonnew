95db1f68a0a80fde5271b7e1f82507e7fd26be6b
vladisvpro <vladik17104@gmai.com>
2020-09-16 22:07:19 +0300

    nakonechto

---
+++ xzkak.py
@@ -0,0 +1,26 @@
+def kaktakto(n):
+
+
+nums = [45, 55, 60, 37, 100, 105, 220]
+
+
+guess = int(input('Введите число на которое хотите поделить : '))
+
+
+if n % guess == 0 :
+    print('все ок')
+    print(nums[n] / guess)
+
+elif n % guess <= 0 :
+    print('Нельзя поделить')
+    print(nums[n] / guess)
+
+else:
+    print('Nельзя поделить')
+
+
+
+
+
+
+
